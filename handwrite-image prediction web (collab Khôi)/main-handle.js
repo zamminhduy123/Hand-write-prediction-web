df081b879577153cb9ae37b553f3f7eea7e3484c
zamminhduy123 <47093479+zamminhduy123@users.noreply.github.com>
2020-03-07 16:02:21 +0700

    Add Drawing section

--- handwrite-image prediction web (collab Khôi)/main-handle.js
+++ handwrite-image prediction web (collab Khôi)/main-handle.js
@@ -1,18 +1,22 @@
 const realFileButton = document.getElementById('input-file')
 const uploadButton = document.getElementById('upload-button')
 const userImg = document.getElementById('user-image')
+const drawButton = document.getElementById('draw-button')
+const cvDrawingBoard = document.getElementById('drawing-board')
+const drawingDir = document.getElementById('drawing-direction');
 
 uploadButton.addEventListener("click", () => {
     realFileButton.click();
 })
 function handleFiles(files) {
+    cvDrawingBoard.hidden = true;
     const reader = new FileReader()
     reader.onload = () => {
         userImg.onload = () => {
             const canvas = document.createElement('canvas')
             const context = canvas.getContext('2d')
                     
-            var MAX_WIDTH =600;
+            var MAX_WIDTH = 600;
             var MAX_HEIGHT = 600;
             var width = userImg.width;
             var height = userImg.height;
@@ -44,11 +48,6 @@ function handleFiles(files) {
                 data[i+2]=avg;
             }
             context.putImageData(imgData,0,0)
-
-            
-
-            
-
             userImg.src = canvas.toDataURL('image/jpeg', 1.0)
             userImg.hidden = false;
         }
@@ -71,3 +70,50 @@ document.addEventListener('drop',function(e){
     e.stopPropagation();
     handleFiles(e.dataTransfer.files);
 },false)
+
+drawButton.addEventListener('click', () => {
+    cvDrawingBoard.hidden = false;
+    userImg.hidden = true;
+    drawingDir.hidden = false;
+    const ctx = cvDrawingBoard.getContext('2d');
+
+    
+
+    cvDrawingBoard.height = 300;
+    cvDrawingBoard.width = 300;
+
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    ctx.rect(0, 0, 300, 300);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.beginPath();
+
+    let drawing = false;
+    function startPos (e) {
+        drawing = true;
+        draw(e);
+    }
+    function endPos(){
+        drawing = false;
+        ctx.beginPath();
+    }
+
+    function draw(e){
+        if (!drawing){
+            return;
+        }
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = 20;
+        ctx.lineCap = 'round';
+        ctx.lineTo(e.clientX - 180,e.clientY - 146);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX - 180,e.clientY  - 146);
+    }
+
+    cvDrawingBoard.addEventListener('mousedown',startPos);
+    cvDrawingBoard.addEventListener('mouseup',endPos);
+    cvDrawingBoard.addEventListener('mousemove',draw);
+
+});
